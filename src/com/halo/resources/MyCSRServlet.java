170ffe0b35889d03bb711b8c39c7ac2a00c8ddbc
Chris Bolton <cdbolton@uga.edu>
2014-08-10 16:32:36 -0400

    initial commit
    initial commit

---
+++ src/com/halo/resources/MyCSRServlet.java
@@ -0,0 +1,67 @@
+package com.halo.resources;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+
+@Path("/myCSR")
+public class MyCSRServlet {
+
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/gamertag")
+	public String getGamertagInfo(@DefaultValue("Gl4di4torRr") @QueryParam("gamertag") String gamertag){
+		ArrayList<String> returnList = new ArrayList<String>();
+		PlayerProfile pp = new PlayerProfile();
+		ParseHelper helper = new ParseHelper();
+		
+		String playlist = "";
+		String rank = "";
+		Document doc = null;
+		try {
+			doc = Jsoup.connect("https://stats.svc.halowaypoint.com/en-US/players/"+ gamertag + "/h4/servicerecord?_=1383093738953").get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		doc = Jsoup.parse(doc.toString(), "", Parser.xmlParser());
+		
+		for(Element e: doc.select("SkillRank")){
+			
+			Elements playListName = e.select("playlistname");
+			for(Element playList: playListName){
+				playlist = playList.toString();
+			}
+			
+			Elements rankInPlayList = e.select("currentskillrank");
+			for(Element rankIncoming: rankInPlayList){
+				rank = rankIncoming.toString();
+			}
+			
+			playlist = helper.parsePlayList(playlist);
+			rank = helper.parseRank(rank);
+			
+			pp.addPlayList(playlist);
+			pp.addRank(rank);
+		}
+		
+		returnList = helper.combineLists(pp, pp.getPlayList(), pp.getRankList());
+		return returnList.toString();
+		
+	}
+	
+	
+}
---
+++ src/com/halo/resources/PlayerProfile.java
@@ -0,0 +1,32 @@
+package com.halo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PlayerProfile {
+	
+	private List<String> rankList;
+	private List<String> playList;
+	
+	public PlayerProfile(){
+		this.rankList = new ArrayList<String>();
+		this.playList = new ArrayList<String>();
+	}
+	
+	public void addRank(String rank){
+		rankList.add(rank);
+	}
+	
+	public List<String> getRankList(){
+		return this.rankList;
+	}
+	
+	public void addPlayList(String playlist){
+		playList.add(playlist);
+	}
+	
+	public List<String> getPlayList(){
+		return this.playList;
+	}
+}
