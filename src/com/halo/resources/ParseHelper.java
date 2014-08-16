8f004f6ea72ddd93dbf5b7fd5786bb439c200b34
William Bolton <wbolton7@gatech.edu>
2014-08-16 05:41:47 -0400

    added .gitignore, a couple changes to index.html, got bored and did some checkstyle fixes

--- src/com/halo/resources/ParseHelper.java
+++ src/com/halo/resources/ParseHelper.java
@@ -10,69 +10,69 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 public class ParseHelper {
-	
 	/**
 	 * Empty default constructor
 	 */
-	public ParseHelper(){}
-	
+	public ParseHelper() { }
+
 	/**
 	 * Returns a list of the users playlists
 	 * @param playlist
 	 * @return
 	 */
-	public String parsePlayList(String playlist){
+	public String parsePlayList(String playlist) {
 		return playlist.substring(14, playlist.length() - 15).trim();
 	}
-	
+
 	/**
 	 * Returns a list sthe users ranks
 	 * @param rank
 	 * @return
 	 */
-	public String parseRank(String rank){
-		if(rank.trim().equals("<currentskillrank i:nil=\"true\" />")){
+	public String parseRank(String rank) {
+		if (rank.trim().equals("<currentskillrank i:nil=\"true\" />")) {
 			return "0";
 		}
 		return rank.substring(18, rank.length() - 19);
 	}
-	
+
 	/**
 	 * Returns the players stats wrapped in the PlayerProfile object
 	 * @param gamertag
 	 * @return
 	 * @throws IOException
 	 */
-	public PlayerProfile getPlayerStats(String gamertag) throws IOException{
+	public PlayerProfile getPlayerStats(String gamertag)
+			throws IOException {
 		PlayerProfile pp = new PlayerProfile();
-	
+
 		String playlist = "";
 		String rank = "";
-		Document doc = Jsoup.connect("https://stats.svc.halowaypoint.com/en-US/players/"+ gamertag + "/h4/servicerecord?_=1383093738953").get();
+		Document doc = Jsoup.connect("https://stats.svc.halowaypoint.com/en-US/players/" + gamertag + "/h4/servicerecord?_=1383093738953").get();
 		doc = Jsoup.parse(doc.toString(), "", Parser.xmlParser());
-		
-		for(Element e: doc.select("SkillRank")){
-			
+
+		for (Element e: doc.select("SkillRank")) {
+
 			Elements playListName = e.select("playlistname");
-			for(Element playList: playListName){
+			for (Element playList: playListName) {
 				playlist = playList.toString();
 			}
-			
+
 			Elements rankInPlayList = e.select("currentskillrank");
-			for(Element rankIncoming: rankInPlayList){
+			for (Element rankIncoming: rankInPlayList) {
 				rank = rankIncoming.toString();
 			}
-			
+
 			playlist = parsePlayList(playlist);
 			rank = parseRank(rank);
-			
+
 			pp.addPlayList(playlist);
 			pp.addRank(rank);
 		}
-		
+
 		return pp;
 	}
-	
+
 	/**
 	 * Combines the two lists in the player profile
 	 * @param pp
@@ -80,21 +80,23 @@ public class ParseHelper {
 	 * @param list2
 	 * @return
 	 */
-	public ArrayList<String> combineLists(PlayerProfile pp, List<String> list, List<String> list2){
+	public ArrayList<String> combineLists(PlayerProfile pp,
+			List<String> list, List<String> list2) {
 		ArrayList<String> combinedList = new ArrayList<String>();
 		int playlistCounter = 0;
 		int rankCounter = 0;
-		for(int i = 0; i < pp.getPlayList().size() * 2; i++){
-			if(i % 2 == 0){
-				combinedList.add(pp.getPlayList().get(playlistCounter));
+		for (int i = 0; i < pp.getPlayList().size() * 2; i++) {
+			if (i % 2 == 0) {
+				combinedList.add(pp.getPlayList().
+						get(playlistCounter));
 				playlistCounter++;
-			}
-			else{
-				combinedList.add(pp.getRankList().get(rankCounter));
+			} else {
+				combinedList.add(pp.getRankList().
+						get(rankCounter));
 				rankCounter++;
 			}
 		}
-		
+
 		return combinedList;
 	}
 
--- src/com/halo/resources/PlayerProfile.java
+++ src/com/halo/resources/PlayerProfile.java
@@ -5,28 +5,28 @@ import java.util.List;
 
 
 public class PlayerProfile {
-	
+
 	private List<String> rankList;
 	private List<String> playList;
-	
-	public PlayerProfile(){
+
+	public PlayerProfile() {
 		this.rankList = new ArrayList<String>();
 		this.playList = new ArrayList<String>();
 	}
-	
-	public void addRank(String rank){
+
+	public void addRank(String rank) {
 		rankList.add(rank);
 	}
-	
-	public List<String> getRankList(){
+
+	public List<String> getRankList() {
 		return this.rankList;
 	}
-	
-	public void addPlayList(String playlist){
+
+	public void addPlayList(String playlist) {
 		playList.add(playlist);
 	}
-	
-	public List<String> getPlayList(){
+
+	public List<String> getPlayList() {
 		return this.playList;
 	}
 }
